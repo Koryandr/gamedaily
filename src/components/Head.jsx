716516c350944ca3aa5ff01c7dc52f398739d593
Kory <kory.tashim@icloud.com>
2025-11-18 17:45:08 +0500

    Сделал "скелет"

---
+++ src/components/Head.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Head = () => {
+    return(
+        <div>
+            <h1>GameDaily</h1>
+            <h3>Ежедневные игровые новости</h3>
+        </div>
+    )
+}
+
+export default Head;
